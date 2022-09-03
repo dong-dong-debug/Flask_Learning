1cbd9c53b7a1941f9b09a226712667c2a4371015
dong-dong-debug <101547577+dong-dong-debug@users.noreply.github.com>
2022-09-03 20:38:11 +0800

    pull commit

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import os
 app = Flask(__name__)
 app.config.from_object(config)
 app.config['SECRET_KEY'] = os.urandom(24)  # 生成随机数种子，用于产生sessionID
-# app.config['SECRET_KEY'] = os.urandom(24)  # 生成随机数种子，用于产生sessionID
-# app.config['SECRET_KEY'] = os.urandom(24)  # 生成随机数种子，用于产生sessionID
 
 # 实例化db对象
 db = SQLAlchemy(app)
